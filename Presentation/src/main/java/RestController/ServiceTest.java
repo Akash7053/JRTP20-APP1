91b2e47fcce2613d722299bac321d6ea150fca06
Akash7053 <aakashssingh9988@gmail.com>
2023-03-22 16:34:45 +0530

    Project commit

---
+++ Presentation/src/main/java/RestController/ServiceTest.java
@@ -0,0 +1,14 @@
+package RestController;
+
+import java.util.ArrayList;
+
+public class ServiceTest {
+	public static void main(String[] args) {
+		ArrayList<String> al =  new ArrayList<>();
+		al.add("akash");
+		al.add("prakash");
+		System.out.println(al);
+		
+	}
+
+}
